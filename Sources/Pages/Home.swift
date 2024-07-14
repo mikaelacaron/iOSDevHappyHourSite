8a9b22258949c87fd9313fb235480979faf3186c
Mikaela Caron <mikaelacaron@gmail.com>
2024-07-14 18:40:49 -0400

    Make basic Homepage

--- Sources/Pages/Home.swift
+++ Sources/Pages/Home.swift
@@ -5,11 +5,25 @@ struct Home: StaticPage {
     var title = "Home"
 
     func body(context: PublishingContext) -> [BlockElement] {
-        NavigationBar(logo: "My Awesome Site") {
-            Link("Links", target: Links())
+        Text("About Us")
+            .font(.title1)
+            .horizontalAlignment(.center)
+        
+        Divider()
+            .foregroundStyle(.red)
+        
+        Text {
+            "The goal of "
+            
+            Link("#iOSDevHappyHour", target: "https://x.com/search?q=%23iOSDevHappyHour&src=hashtag_click&f=live")
+            
+            "is to allow current and aspiring iOS Developers a place to share experiences, share knowledge, simply hang out, build community, and even enjoy a drink or two online during a time where face to face contact is limited."
         }
         
-        Text("Hello world!")
-            .font(.title1)
+        Text {
+            Link("Register", target: "")
+                .linkStyle(.button)
+                .role(.primary)
+        }
     }
 }
