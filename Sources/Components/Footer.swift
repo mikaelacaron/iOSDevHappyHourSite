7e0e05280efab0febbe380d1966fd70f80051923
Mikaela Caron <mikaelacaron@gmail.com>
2024-08-01 22:33:00 -0400

    Make a better social footer and make the links page take up less width

--- Sources/Components/Footer.swift
+++ Sources/Components/Footer.swift
@@ -15,6 +15,42 @@ struct Footer: Component, HTMLRootElement {
         }
         .horizontalAlignment(.center)
         .margin(.top, .extraLarge)
-        .class("container-fluid")
+    }
+}
+
+public struct SocialFooter: Component, HTMLRootElement {
+    let icons = [
+      Image(systemName: "twitter"),
+      Image(systemName: "mastodon"),
+      Image(systemName: "threads"),
+      Image(systemName: "youtube"),
+      Image(systemName: "github")
+    ]
+
+    let urlStrings = [
+      "https://x.com/iosdevhappyhour",
+      "https://mastodon.iosdevhappyhour.com/@iosdevhappyhour",
+      "https://threads.net/@iosdevhappyhour",
+      "https://youtube.com/@iOSDevHappyHour",
+      "https://github.com/mikaelacaron"
+    ]
+
+    public func body(context: PublishingContext) -> [any PageElement] {
+        Text {
+            for (icon, urlString) in zip(icons, urlStrings) {
+                Link(icon, target: urlString)
+                    .margin(.trailing, 20)
+                    .style("color: white")
+                    .class("fs-1")
+                    .target(.blank)
+                    .relationship(.noOpener, .noReferrer)
+            }
+        }
+        .font(.title2)
+        .padding(50)
+        .horizontalAlignment(.center)
+        .margin(.top, .extraLarge)
+        .margin(.bottom, 0)
+        .background(.init(hex: "#204161"))
     }
 }
