2c99bca78509a7dd410ab77864bdb311138b0bb9
Mikaela Caron <mikaelacaron@gmail.com>
2024-08-01 23:00:43 -0400

    Make a LinkItem and linkCard

---
+++ Sources/Models/LinkItem.swift
@@ -0,0 +1,15 @@
+//
+//  LinkItem.swift
+//
+//
+//  Created by Mikaela Caron on 8/1/24.
+//
+
+import Foundation
+
+struct LinkItem {
+    let header: String
+    let imageName: String
+    let text: String
+    let url: URL
+}
--- Sources/Pages/Links.swift
+++ Sources/Pages/Links.swift
@@ -1,4 +1,5 @@
 import Ignite
+import Foundation
 
 struct Links: StaticPage {
     var title = "Links"
@@ -14,98 +15,73 @@ struct Links: StaticPage {
         .margin(50)
         
         Section {
-            Card {
-                Text {
-                    Image(systemName: "calendar")
-                        .margin(.trailing, 20)
-                        .foregroundStyle(.init(hex: "#204161"))
-                    
-                    "RSVP for the Zoom Link!"
-                }
-                .font(.title1)
-            } header: {
-                Text {
-                    "This Month's Event"
-                }
-                .fontWeight(.bold)
-            }
-            .cardStyle(.bordered)
-            .margin(.bottom, 20)
+            linkCard(
+                LinkItem(
+                    header: "This Month's Event",
+                    imageName: "calendar",
+                    text: "RSVP for the Zoom Link!",
+                    url: URL(string: "https://idhh-july2024.eventbrite.com/")!
+                )
+            )
             
-            Card {
-                Text {
-                     Image(systemName: "discord")
-                        .margin(.trailing, 20)
-                        .foregroundStyle(.blue)
-
-                    Link("iOSDevHappyHour Discord", target: "https://discord.com/invite/FEtB3b247B?mc_cid=f2c2a15a92")
-                }
-                .font(.title1)
-            } header: {
-                Text {
-                    "Community"
-                }
-                .fontWeight(.bold)
-            }
-            .cardStyle(.bordered)
-            .margin(.bottom, 20)
+            linkCard(
+                LinkItem(
+                    header: "Community",
+                    imageName: "discord",
+                    text: "iOSDevHappyHour Discord",
+                    url: URL(string: "https://discord.com/invite/FEtB3b247B?mc_cid=f2c2a15a92")!
+                )
+            )
             
-            Card {
-                Text {
-                    Image(systemName: "balloon-heart")
-                        .margin(.trailing, 20)
-                        .foregroundStyle(.blue)
-                    
-                    Link("Buy Us a Coffee", target: "https://www.buymeacoffee.com/iosdevhappyhour")
-                }
-                .font(.title1)
-            } header: {
-                
-                Text {
-                    "Donation"
-                }
-                .fontWeight(.bold)
-            }
-            .cardStyle(.bordered)
-            .margin(.bottom, 20)
+            linkCard(
+                LinkItem(
+                    header: "Donation",
+                    imageName: "balloon-heart",
+                    text: "Buy Us a Coffee",
+                    url: URL(string: "https://www.buymeacoffee.com/iosdevhappyhour")!
+                )
+            )
             
-            Card {
-                Text {
-                    Image(systemName: "envelope-open")
-                        .margin(.trailing, 20)
-                        .foregroundStyle(.blue)
-                    
-                    Link("Sponsorships", target: "https://sponsorships.iosdevhappyhour.com/")
-                }
-                .font(.title1)
-            } header: {
-                Text {
-                    "Support"
-                }
-                .fontWeight(.bold)
-            }
-            .cardStyle(.bordered)
-            .margin(.bottom, 20)
+            linkCard(
+                LinkItem(
+                    header: "Support",
+                    imageName: "envelope-open",
+                    text: "Sponsorships",
+                    url: URL(string: "https://sponsorships.iosdevhappyhour.com/")!
+                )
+            )
             
-            Card {
-                Text {
-                    Image(systemName: "exclamation-circle-fill")
-                        .margin(.trailing, 20)
-                        .foregroundStyle(.blue)
-                    
-                    Link("Code of Conduct", target: "https://docs.google.com/presentation/d/1uHLuuDHrRXxlnYYMAR5lYJ2POZM2JxZChebiMeXdXd0/edit#slide=id.p")
-                }
-                .font(.title1)
-            } header: {
-                Text {
-                    "Support"
-                }
-                .fontWeight(.bold)
-            }
-            .cardStyle(.bordered)
-            .margin(.bottom, 20)
+            linkCard(
+                LinkItem(
+                    header: "Support",
+                    imageName: "exclamation-circle-fill",
+                    text: "Code of Conduct",
+                    url: URL(string: "https://docs.google.com/presentation/d/1uHLuuDHrRXxlnYYMAR5lYJ2POZM2JxZChebiMeXdXd0/edit#slide=id.p")!
+                )
+            )
         }
         .columns(1)
         .class("col-sm-8", "mx-auto")
+    }
+    
+    func linkCard(_ item: LinkItem) -> BlockElement {
+        Card {
+            Text {
+                Image(systemName: item.imageName)
+                    .class("fs-1")
+                    .margin(.trailing, 20)
+                    .foregroundStyle(.init(hex: "#204161"))
+                
+                Link(item.text, target: item.url.absoluteString)
+            }
+            .font(.title1)
+        } header: {
+            Text {
+                item.header
+            }
+            .fontWeight(.bold)
+        }
+        .cardStyle(.bordered)
+        .margin(.bottom, 20)
     }
 }
